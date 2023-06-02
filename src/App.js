ab7b0b2cedcbf7360f978852805c2c1faed7106a
oliverbb1 <98846468+oliverbb1@users.noreply.github.com>
2023-06-02 13:57:42 +0200

    create heder component

--- src/App.js
+++ src/App.js
@@ -6,11 +6,14 @@ import Offers from './pages/Offers';
 import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import Header from './components/Header';
+// import Header from './components/Header';
 
 function App() {
   return (
     <>
       <Router>
+        <Header/>
         <Routes>
           <Route path='/' element={<Home/>}></Route>
           <Route path='/forgot-password' element={<ForgotPassword/>}></Route>
